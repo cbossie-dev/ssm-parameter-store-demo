8a249cdc93fd7f6c1ba675ce82083df9baba0d7f
Craig Bossie <cbbossie@amazon.com>
2023-10-13 11:48:26 -0400

    update

--- demo-infrastructure-cdk/src/DemoInfrastructure/Program.cs
+++ demo-infrastructure-cdk/src/DemoInfrastructure/Program.cs
@@ -27,23 +27,23 @@ namespace DemoInfrastructure
                 EcrRepository = ecrRepositoryName,
                 StackName = stackName,
                 EnvironmentPostfix = environmentPostfix,
-                Env = MakeEnv(account, region)
+                //Env = MakeEnv(account, region)
             });
             app.Synth();
         }
 
 
-        private static Amazon.CDK.Environment MakeEnv(string account = null, string region = null)
-        {
-            return new Amazon.CDK.Environment
-            {
-                Account = account ??
-                    System.Environment.GetEnvironmentVariable("CDK_DEPLOY_ACCOUNT") ??
-                    System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
-                Region = region ??
-                    System.Environment.GetEnvironmentVariable("CDK_DEPLOY_REGION") ??
-                    System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION")
-            };
-        }
+        // private static Amazon.CDK.Environment MakeEnv(string account = null, string region = null)
+        // {
+        //     return new Amazon.CDK.Environment
+        //     {
+        //         Account = account ??
+        //             System.Environment.GetEnvironmentVariable("CDK_DEPLOY_ACCOUNT") ??
+        //             System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
+        //         Region = region ??
+        //             System.Environment.GetEnvironmentVariable("CDK_DEPLOY_REGION") ??
+        //             System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION")
+        //     };
+        // }
     }
 }
